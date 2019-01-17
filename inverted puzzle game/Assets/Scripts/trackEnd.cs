feace91da833683960da38a6958ec404c58dfe5a
TaraCarette <terra.the.great@gmail.com>
2019-01-17 13:04:57 -0500

    initial prototype of game, no second phase

---
+++ inverted puzzle game/Assets/Scripts/trackEnd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackEnd : MonoBehaviour
+{
+    public static bool wizardSucess = false;
+    public static bool hatSucess = false;
+
+    private bool done = false;
+
+    
+    public GameObject wizardHatPlayer;
+    public GameObject wizardPlayer;
+    public GameObject hatPlayer;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (wizardSucess && hatSucess && !done) 
+        {
+            Debug.Log("Now!!!!!!!!!");
+            Destroy(wizardPlayer);
+            Destroy(hatPlayer);
+            wizardHatPlayer.SetActive(true);
+            done = true;
+        }
+    }
+}
