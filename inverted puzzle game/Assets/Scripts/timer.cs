d5d1e7b3ccc6b98258d1bee6a98e1731f44c3979
TaraCarette <terra.the.great@gmail.com>
2019-03-06 00:20:48 -0500

    timer script

---
+++ inverted puzzle game/Assets/Scripts/timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+
+public class timer : MonoBehaviour {
+
+  public int timeLeft = 60; //Seconds Overall
+  public Text countdown; //UI Text Object
+
+  private string currentScene;
+
+  void Start () {
+    StartCoroutine("LoseTime");
+    Time.timeScale = 1; //Just making sure that the timeScale is right
+
+    currentScene =  SceneManager.GetActiveScene().name;
+  }
+
+  void Update () {
+    countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+  }
+
+  //Simple Coroutine
+  IEnumerator LoseTime()
+  {
+    while (true) {
+      yield return new WaitForSeconds (1);
+      timeLeft--; 
+
+      if (timeLeft <= 0) {
+        Debug.Log("out of time");
+        SceneManager.LoadScene(currentScene, LoadSceneMode.Single);
+      }
+    }
+  }
+}
