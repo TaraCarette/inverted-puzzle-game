b0a0ef891dabac8489b31b014e83161359241273
Wei Dai <dai4962@gmail.com>
2019-04-01 10:34:13 -0400

    Main Menu complete

---
+++ inverted puzzle game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
