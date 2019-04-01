e7d73e8129c4bfd39c9a788660ef10c2c1496003
TaraCarette <terra.the.great@gmail.com>
2019-03-31 23:11:56 -0400

    music on part b of level can pause

---
+++ inverted puzzle game/Assets/Scripts/notMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notMenu : MonoBehaviour
+{
+    private AudioSource source;
+    private bool pauseMenu;
+    private bool victoryMenu;
+    private bool deathMenu;
+    private bool currentlyPaused = false;
+
+    public GameObject pause;
+    public GameObject victory;
+    public GameObject death;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        source.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!currentlyPaused && (pause.activeSelf || victory.activeSelf || death.activeSelf)) 
+        {
+            Debug.Log("pause music");
+            source.Pause();
+            currentlyPaused = true;
+        }
+
+        if (currentlyPaused && !pause.activeSelf && !victory.activeSelf && !death.activeSelf) 
+        {
+            Debug.Log("unpaused music");
+            source.Play();
+            currentlyPaused = false;
+        }
+    }
+}
